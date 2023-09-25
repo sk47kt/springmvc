eaf53d412af9394eb1c42093ac5b2c2f6e047dd5
sk47kt <sk47kt@naver.com>
2023-09-25 16:17:48 +0900

    init

---
+++ springmvc/src/test/java/sk47kt/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package sk47kt.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
